55759faff37d84f609f7c5d3760b37a8a5f2dc3f
Gabriel Vigil <gabrielv1999@gmail.com>
2021-03-17 11:19:23 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='test_run',
+      version='1.0',
+      description='Import test run',
+      author='Gabriel Vigil',
+      url='https://www.archethought.com',
+      packages=['distutils', 'distutils.command'],
+     )
